684421a955f26304ffc8238b1aca3c842ace221c
Jack <furegin@yandex.ru>
2022-02-27 21:44:03 +0500

    feat: add BrowserLogger

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import {LEVEL_ERROR} from "./LogLevel";
 import Logger from './Logger'
+import ConsoleDriver from "./drivers/ConsoleDriver";
+
+const BrowserLogger = (level: number = LEVEL_ERROR) => new Logger({level: level, driver: new ConsoleDriver()})
 
 export default Logger;
+
+export {
+  BrowserLogger
+};
